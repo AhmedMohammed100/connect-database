05fd4bbeb8edf121db4e315b45e1fb8000061fe2
AhmedMohammed100 <ahmedtal650@gmail.com>
2024-11-04 19:01:31 +0000

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Script from 'next/script'
+import './script.js'
 
 const inter = Inter({ subsets: ['latin'] })
 
